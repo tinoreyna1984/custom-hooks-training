7895cdd7fed80618d21cbb9904952d93e23cd481
Tino Reyna <tinoreyna1984@gmail.com>
2022-06-02 22:52:19 -0500

    Cambios menores

--- useDidMountEffect.js
+++ useDidMountEffect.js
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 /**
  * useDidMountEffect se basa en el componentDidMount de React a través del
  * useEffect más el hook useRef para prevenir el primer render.
+ * Realiza prácticamente lo mismo que useEffect pero de forma más razonable.
  */
 
 export const useDidMountEffect = (func, deps) => {
